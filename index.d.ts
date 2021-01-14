6550a651aa3198179bb795151697a6e842d9a175
Martin Heidegger <martin.heidegger@gmail.com>
2021-01-14 15:35:40 +0900

    docs: adding index.js typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+import bigIntBE from './bigint-be'
+import bigIntLE from './bigint-le'
+import bigIntQuick from './bigint-quick'
+import bigUintBE from './biguint-be'
+import bigUintLE from './biguint-le'
+import { bytesIter, createCodec } from './lib'
+
+export = {
+  bigIntBE,
+  bigIntLE,
+  bigIntQuick,
+  bigUintBE,
+  bigUintLE,
+  bytesIter,
+  createCodec
+}
--- test/index.js
+++ test/index.js
@@ -1,6 +1,21 @@
-require('./bigint-le.js')
-require('./bigint-be.js')
-require('./bigint-quick.js')
-require('./biguint-be.js')
-require('./biguint-le.js')
-require('./lib.js')
+const { test } = require('fresh-tape')
+const bigIntBE = require('../bigint-be')
+const bigIntLE = require('../bigint-le')
+const bigIntQuick = require('../bigint-quick')
+const bigUintBE = require('../biguint-be')
+const bigUintLE = require('../biguint-le')
+const { createCodec, bytesIter } = require('../lib')
+const api = require('../')
+
+test('api', t => {
+  t.deepEquals(api, {
+    bigIntBE,
+    bigIntLE,
+    bigIntQuick,
+    bigUintBE,
+    bigUintLE,
+    createCodec,
+    bytesIter
+  })
+  t.end()
+})
